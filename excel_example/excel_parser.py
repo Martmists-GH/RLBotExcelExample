5d50d16f9be64ffdc9a9ba29db92f75a7562d539
martmists <martmists@gmail.com>
2019-02-26 23:52:31 +0100

    Update excel_parser.py

--- excel_example/excel_parser.py
+++ excel_example/excel_parser.py
@@ -1,13 +1,19 @@
 import re
+import itertools
+from tabulate import tabulate
 import openpyxl
 
 # ===================================================
 # == Implement Excel functions you need below here ==
 # ===================================================
 
-from math import atan2, sqrt as SQRT, degrees as DEGREES, cos as COS, sin as SIN, pow as POWER
+from math import atan2, sqrt as SQRT, degrees as DEGREES, cos as COS, sin as SIN, pow as POWER, radians as RADIANS
 
 ABS = abs
+
+def SUM(args):
+    args = [arg if isinstance(arg, int) else SUM(arg) for arg in args]
+    return sum(args)
 
 def ATAN2(x, y):
     return atan2(y, x)
@@ -20,12 +26,23 @@ class _xlfn:
     def XOR(left, right):
         return (left or right) and not (left and right)
 
-def AND(left, right):
-    return left and right
+def AND(*args):
+    return all(args)
 
+def OR(*args):
+    return any(args)
+    
 def INDEX(arr, y, x):
     new = arr[x-1]
     return new[y-1]
+
+def SIGN(x):
+    if x == 0:
+        return 0
+    if x > 0:
+        return 1
+    if x < 0:
+        return -1
 
 def MATCH(target, items, _):
     i = 0
@@ -35,27 +52,57 @@ def MATCH(target, items, _):
                 return i + 1
             i += 1
     
-    return "#VALUE!"
+    raise Exception(f"Unable to find key {target} in {items}")
     
 def SWITCH(target, *options):
     for k, v in zip(options[::2], options[1::2]):
         if k == target:
             return v
-    return "#VALUE!"
+    raise Exception(f"Unable to find key {target} in {options[::2]}")
+
+def AVERAGE(*items):
+    return sum(items) / len(items)
+    
+def _phase_inner():
+    num = 0
+    while True:
+        num += 1
+        if num > 10:
+            num = 0
+        yield num
+        
+iter_ = _phase_inner()
+    
+def phase(*_):
+    return next(iter_)
 
 def globs():
     return {
         k: v for k, v in globals().items()
-        if k in ("SQRT", "ATAN2", "IF", "_xlfn", 
-                 "AND", "INDEX", "MATCH", "SWITCH",
-                 "DEGREES", "COS", "SIN", "POWER", "ABS")
+        if k in ("SQRT", "ATAN2", "IF", "_xlfn", "phase",
+                 "AND", "INDEX", "MATCH", "SWITCH", "OR",
+                 "DEGREES", "COS", "SIN", "POWER", "ABS",
+                 "RADIANS", "AVERAGE", "SIGN", "SUM")
         # Add your functions here to expose them to Excel
     }
 
 # ===============================================================
 # == DO NOT EDIT BELOW THIS UNLESS YOU KNOW WHAT YOU ARE DOING ==
 # ===============================================================
-    
+
+
+def col_list(x, y):
+    def col_index(char):
+        ord_list = [*map(lambda i: ord(i)-65, char)][::-1]
+        return sum(26**i + j*26**i for i, j in enumerate(ord_list))-1
+    x,y = [*map(col_index, (x, y))]
+    cols = itertools.chain(*[itertools.product(map(chr, range(65, 91)), repeat=i) for i in range(1, 8)])
+    for i, j in enumerate(cols):
+        if x <= i <= y:
+            yield "".join(j)
+        elif i > y:
+            break
+            
 class Cell:
     def __init__(self, cell, parent):
         self.pos = cell.coordinate
@@ -73,12 +120,15 @@ class Cell:
             for sub_cell in re.finditer("[A-Z]+[0-9]+", new_val):
                 name = sub_cell.group(0)
                 if name not in globals():
-                    new =  repr(self.parent[name].evaluate())
+                    new =  repr(self.parent.eval(name))
                     new_val = new_val.replace(name, new)
             
-            new_val = re.sub("([^=])=([^=])", r"\1==\2", new_val)
-            res = eval(new_val, globs())
-            # print(self.value)
+            new_val = re.sub(r"([^\>\<=])=([^\>\<=])", r"\1==\2", new_val)
+            try:
+                res = eval(new_val, globs())
+            except Exception as exc:
+                raise Exception(f"Exception in {self.pos}: {exc}")
+            
             # print(f"[{self.pos}] ={new_val} |=> {res}")
             return res
         return self.value
@@ -86,6 +136,7 @@ class Cell:
 class Workbook:
     def __init__(self, cells):
         self.cn = {}
+        self.cache = {}
         for col in cells:
             for cell in col:
                 self.new_cell(cell)
@@ -98,6 +149,16 @@ class Workbook:
     
     def __setitem__(self, key, value):
         self.cn[key].value = value
+    
+    def eval(self, item):
+        if item in self.cache:
+            return self.cache[item]
+        res = self[item].evaluate()
+        self.cache[item] = res
+        return res
+    
+    def clear(self):
+        self.cache = {}
 
     def replace_range(self, range_):
         match = re.match(r"(?P<chars_start>[A-Z]+)(?P<ints_start>[0-9]+):(?P<chars_end>[A-Z]+)(?P<ints_end>[0-9]+)", range_)
@@ -108,15 +169,26 @@ class Workbook:
         
         cells = []
         
-        for c in range(ord(start_c), ord(end_c)+1):
+        for c in col_list(start_c, end_c):
             row = []
             for i in range(start_i, end_i+1):
-                cell = chr(c) + str(i)
-                row.append(self[cell].evaluate())
+                cell = c + str(i)
+                row.append(self.eval(cell))
             cells.append(row)
         return cells
     
+    def __repr__(self):
+        text = ""
+        table = []
+        for row in range(1, 82):
+            r = []
+            for col in col_list("A", "AB"):
+                r.append(self.eval(col+str(row)) or "")
+            table.append(r)
+        return tabulate(table)
+    
     def eval_stmt(self, stmt):
+        # FIXME
         return Cell(stmt, self).evaluate()
 
 # Entry point
